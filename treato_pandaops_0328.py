ba9bb15df9e2e95f2c7d88925b266a9e46047c2e
patrickschu <patrickschu@users.noreply.github.com>
2016-03-29 14:21:07 -0500

    Added files via upload

--- treato_pandaops_0328.py
+++ treato_pandaops_0328.py
@@ -2,9 +2,8 @@ import pandas,os
 os.chdir(os.path.join("/Users","ps22344","Documents"))
 one=pandas.read_csv("healthoutput_0328.csv")
 print one.columns
-#print one
 two=pandas.read_csv("treato_LIWC2015.csv")
 print two.columns
 merged=one.merge(two,on="Filename")
 print len(merged)
-merged.to_csv('test.csv')
+merged.to_csv('LWIC_and_onto_merged_0328.csv')
