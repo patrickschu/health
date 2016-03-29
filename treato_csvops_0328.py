bf64e87d00235800992dbdb5aef08e4027133ecc
patrickschu <patrickschu@users.noreply.github.com>
2016-03-29 11:59:46 -0500

    Added files via upload

--- treato_csvops_0328.py
+++ treato_csvops_0328.py
@@ -1,6 +1,6 @@
 import csv
-inputi=open("treato_LIWC2015.csv", "r")
-outputi=open("treato_lwic_over_50.csv", "w")
+inputi=open("LWIC_and_onto_merged_0328.csv", "r")
+outputi=open("LWIC_and_onto_merged_over50_0328.csv", "w")
 
 
 outputrows=[]
@@ -8,15 +8,15 @@ reader=csv.reader(inputi, dialect="excel")
 
 #get header out of the way
 header=reader.next()
-
+print header[2]
 
 #item[2] is the wordcount
 for line in reader:
-	if float(line[2]) > 49:
-		outputrows.append(line)
-
+ 	if float(line[2]) > 49:
+ 		outputrows.append(line)
+ 
 print "the outputfile has {} rows".format(len(outputrows))
-
+# 
 print "writing"
 writer=csv.writer(outputi, dialect="excel")
 writer.writerow(header)
