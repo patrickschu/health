51683851290bf836000303db5eb481b7a93be302
patrickschu <patrickschu@users.noreply.github.com>
2016-03-29 11:59:05 -0500

    Added files via upload

--- feature_extraction_ontology_0118.py
+++ feature_extraction_ontology_0118.py
@@ -1,12 +1,12 @@
-import os
-import re
+import os, re, nltk, string, codecs, csv
+#from n
 
 print "start"
 #read in each file
+
 inputfiles=os.listdir("outputfiles_speakers")
 #inputfiles=["usmessageboard.com_OnePercenter.txt", "facebook.com_10206966179415036.txt", "061006_*guest.txt"]
 print "number of files", len(inputfiles)
-
 
 # tagextracter takes the inputfile and any kind of tag that 
 # encloses the text to be extracted
@@ -17,7 +17,7 @@ def tagextracter(inputtext, tag, fili):
 	if len(result) > 1:
 		print tag 
 		print result[1]
-		print "alarm!! extracter has found more than 1 result"
+		print "alarm!! tag extracter has found more than 1 result\n----\n\n"
 	if len(result) < 1: 
 		print fili
 		print tag
@@ -36,51 +36,71 @@ def featureextracter(inputtext, search_term, fili):
 	
 
 #set up csv file
-filename="healthoutput_1118"
+filename="healthoutput_0328"
 outputfile=open(filename+".csv", "a")
 logfile=open(filename+"_log.csv", "a")
-
-
+writer=csv.writer(outputfile, dialect="excel")
+header=['Filename','wordcount', 'think', 'believe', 'wonder', 'know', 'thinkfreq', 'believefreq', 'wonderfreq', 'knowfreq', 'ontofreq']
+writer.writerow(header)
 
 #extract text and speakers for csv file
 for fili in inputfiles:
 	try:
 		#set up the inputfile
-		inputfile=open("outputfiles_speakers/"+fili)
+		inputfile=open("outputfiles_speakers/"+fili, "r")
 		inputtext=inputfile.read()
 	
 		#get the speaker name
 		speaker=tagextracter(inputtext, "speaker", fili)
 		#extract text&count words
 		text=tagextracter(inputtext, "text", fili)
-		splittext=text.split()
-		wordcount=len(splittext)
+		#splittext=nltk.wordpunct_tokenize(text)
+		splittext=[i for i in nltk.wordpunct_tokenize(text)  if i not in string.punctuation]
+		wordcount=float(len(splittext))
+		#we;re not worrying about punctuation as in this context we don't expect any
 	
-		#get the variables
-		#note that we can add e.g. third person forms
-		#or e.g. exclude negation and focus on 1st person only
-		think=featureextracter(text, "(think|thought)", fili)
+		# #get the variables
+# 		#note that we can add e.g. third person forms
+# 		#or e.g. exclude negation and focus on 1st person only
+ 		think=featureextracter(text, "(think|thought)", fili)
 		believe=featureextracter(text, "(believe|believed)", fili)
 		wonder=featureextracter(text, "(wonder|wondered)", fili)
 		know=featureextracter(text, "(know|knew|known)", fili)
+		outputvalues=[len(think), len(believe), len(wonder), len(know)]
+		outputfloats=[float(i) for i in outputvalues]
+		outputfreqs=[i / wordcount for i in outputfloats]
+		overallfreq=sum(outputfloats)/wordcount
+		final=[speaker]+[wordcount]+outputfloats+outputfreqs+[overallfreq]
 	
-		#write up the csv file
-		outputfile.write(speaker+","+
-		str(wordcount)+","+
-		str(len(think))+
-		","+str(len(believe))+
-		","+str(len(wonder))+
-		","+str(len(know))+"\n")
+		# #write up the csv file
+# 		outputfile.write(speaker+","+
+# 		str(wordcount)+","+
+# 		",".join([str(i) for i in outputfloats])+","+
+# 		",".join([str(i) for i in outputfreqs])+","+
+# 		str(overallfreq)+"\n")
+		
+# 		#write up the csv file
+# 		outputfile.write(speaker+"\t"+
+# 		str(wordcount)+"\t"+
+# 		"\t".join([str(i) for i in outputfloats])+"\t"+
+# 		"\t".join([str(i) for i in outputfreqs])+"\t"+
+# 		str(overallfreq)+"\n")
+		
+		
+#write up the csv file
+ 		
+ 		writer.writerow(final)
+ 		
 	
 		#and the log
-		logfile.write(speaker+","+
+		logfile.write(speaker+".txt,"+
 		" ".join(think)+","+
 		" ".join(believe)+","+
 		" ".join(wonder)+","+
 		" ".join(know)+"\n")
 		
-	except: 
-		print "Alarm", fili
+	except Exception, e: 
+		print "Alarm", fili, e
 
 outputfile.close()
 logfile.close()
